5efbf57188d36989f5fa9c105a2d7394d24ca7db
Khubaib <khubaibwasim75@gmail.com>
2024-04-11 23:08:14 +0500

    update 2

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,41 +1,35 @@
 import prisma from "@/prisma";
 import { NextResponse } from "next/server";
 
-export async function main(){
-    try {
-        await prisma.$connect()
-    } catch (error) {
-        return Error("Error connecting")
-    }
+export async function main() {
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error("Database Connection Unsuccessull");
+  }
 }
 
-export const GET = async(req:Request, res:NextResponse)=>{
-    try {
-        await main()
-        const Posts = await prisma.post.findMany()
-        return NextResponse.json({message:"Success", Posts}, {status: 200})
-    } catch (error) {
-        return NextResponse.json({message:error}, {status:500})
-    }finally{
-        await prisma.$disconnect()
-    }
-}
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    await main();
+    const posts = await prisma.post.findMany();
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
 
-export const POST = async(req:Request, res:NextResponse)=>{
-    try {
-        const {title, description} = await req.json()
-        await main()
-        const post = await prisma.post.create({
-            data:{
-                title,
-                description,
-            }
-        })
-
-        return NextResponse.json({message:"Success", post}, {status: 200})
-    } catch (error) {
-        return NextResponse.json({message:error}, {status:500})
-    }finally{
-        await prisma.$disconnect()
-    }
-}
+export const POST = async (req: Request, res: NextResponse) => {
+  try {
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.create({ data: { description, title } });
+    return NextResponse.json({ message: "Success", post }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
